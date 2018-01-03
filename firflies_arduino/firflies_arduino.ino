1866e5372716680cf66c067f0b733c0c320bafca
ThijsMannens <35066006+ThijsMannens@users.noreply.github.com>
2018-01-03 17:11:36 +0100

    Add files via upload

---
+++ firflies_arduino/firflies_arduino.ino
@@ -0,0 +1,51 @@
+/*
+  Melody
+
+ Plays a melody
+
+ circuit:
+ * 8-ohm speaker on digital pin 8
+
+ created 21 Jan 2010
+ modified 30 Aug 2011
+ by Tom Igoe
+
+This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/Tone
+
+ */
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+  NOTE_D4, NOTE_A4, NOTE_A4, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_D4, 0, NOTE_D4, NOTE_E4, NOTE_E4, NOTE_D4, NOTE_E4, NOTE_G4, NOTE_A4, 0, NOTE_B4, NOTE_B4, NOTE_A4, NOTE_G4 , NOTE_D4, NOTE_D4, NOTE_A4, NOTE_G4, NOTE_A4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  5, 5, 5, 6, 6, 5, 6,6,6,6,6,8,6
+};
+
+void setup() {
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 25; thisNote++) {
+
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
+}
+
+void loop() {
+  // no need to repeat the melody.
+}
